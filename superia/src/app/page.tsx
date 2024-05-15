a4fe3b30e41c51d6c78c0f62c29c7cc1efd1e269
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-05-15 11:52:08 +0200

    Update 2.3

--- superia/src/app/page.tsx
+++ superia/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 	  </ul>
 		</nav>
     <h1 className="text-4xl font-semibold text-center py-7">Découvrez Superia,</h1>
-    <p className="text-center py-2.5 mx-32 px-32">"La première application qui utiliser l'IA générative pour l'analyse de marque employeur. Grâce à Superia, vous pouvez désormais exploiter pleinement le potentiel de votre site carrière, en analysant et optimisant leur contenu pour attirer les meilleurs talents."</p>
+    <p className="text-center py-2.5 mx-32 px-32">La première application qui utiliser lIA générative pour lanalyse de marque employeur. Grâce à Superia, vous pouvez désormais exploiter pleinement le potentiel de votre site carrière, en analysant et optimisant leur contenu pour attirer les meilleurs talents.</p>
     <div className="mx-auto text-2xl">
     <button className="p-5 pl-20 pr-20 m-10 rounded-md border-0  text-blue-900 ring-1 ring-inset ring-blue-300 2xl:text-2xl 2xl:leading-8"><Link href={"scrapping"}>ScanRH</Link></button>
     <button className="p-5 pl-20 pr-20 m-10 rounded-md border-0  text-blue-900 ring-1 ring-inset ring-blue-300 2xl:text-2xl 2xl:leading-8"><Link href={"parsing"}>DocuParse</Link></button>
