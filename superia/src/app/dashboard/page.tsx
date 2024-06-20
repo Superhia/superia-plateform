e4bb8c62b27bf9d606a77ea76d22dfae76712bc9
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-06-20 11:55:03 +0200

    Update 4.4

--- superia/src/app/dashboard/page.tsx
+++ superia/src/app/dashboard/page.tsx
@@ -1,9 +1,20 @@
 import AdminRegister from '../components/AdminRegister';
 import UserList from '../components/UserList';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex text-black min-h-screen flex-col items-left p-14 bg-white">
+      <Link href={"/"}>
+        <img src="LaSuperAgence.png" alt="icon" className="h-8" />
+      </Link>
+      <nav className="flex">
+        <ul className="flex space-x-5 mx-auto -mt-5" id="menu">
+          <li><Link href={"scrapping"}>ScanRH</Link></li>
+          <li><Link href={"parsing"}>DocuParse</Link></li>
+          <li><Link href={"chatpdf"}>YoutubeScan</Link></li>
+        </ul>
+      </nav>
     <div>
       <h1 className="text-4xl font-semibold text-center py-7">Bienvenue dans votre system de management</h1>
       <UserList />
