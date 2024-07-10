804ec37a74bcb1e1d6e99ebc1dad2973af63a0e1
Dalvik <99361351+DalvikLoger@users.noreply.github.com>
2024-07-10 11:38:07 +0200

    Update 5.0

--- superia/src/app/components/login/Login.tsx
+++ superia/src/app/components/login/Login.tsx
@@ -1,36 +1,26 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import ReCAPTCHA from 'react-google-recaptcha';
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const [recaptchaToken, setRecaptchaToken] = useState('');
   const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
   const router = useRouter();
-  const handleRecaptchaChange = (token: string | null) => {
-    if (token) {
-      setRecaptchaToken(token);
-    }
-  };
-  const handleLoginSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!recaptchaToken) {
-      setError('Please complete the reCAPTCHA');
-      return;
-    }
     try {
       const res = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password, recaptchaToken }),
+        body: JSON.stringify({ email, password }),
       });
+
       if (res.ok) {
-        setSuccess('Login successful!');
+        console.log('Login successful, navigating to home page');
         router.push('/');
       } else {
         const data = await res.json();
@@ -40,69 +30,31 @@ const Login = () => {
       setError('Something went wrong');
     }
   };
-  const handleSendConfirmationEmail = async () => {
-    try {
-      const res = await fetch('/api/auth/send-confirmation-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
-      if (res.ok) {
-        setSuccess('Confirmation email sent!');
-      } else {
-        const data = await res.json();
-        setError(data.message);
-      }
-    } catch (error) {
-      setError('Something went wrong');
-    }
-  };
+
   return (
-    <div>
-      <form className='container' onSubmit={handleLoginSubmit}>
-        <input
-          className='block w-full mb-5 rounded-md border-0 py-2.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-xl 2xl:leading-6'
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <div className='relative mb-5'>
-          <input
-            className='block w-full rounded-md border-0 py-2.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-xl 2xl:leading-6'
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-          />
-          <button
-            type="button"
-            className='absolute inset-y-0 right-0 flex items-center px-4 text-gray-600'
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? 'Hide' : 'Show'}
-          </button>
-        </div>
-        <ReCAPTCHA
-          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
-          onChange={handleRecaptchaChange}
-        />
-        <button
-          className='p-5 pl-20 pr-20 m-5 mx-40 rounded-md border-0 text-blue-900 ring-1 ring-inset ring-blue-300 text-xl 2xl:leading-8'
-          type="submit"
-        >
-          Login
-        </button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {success && <p style={{ color: 'green' }}>{success}</p>}
-      </form>
-      
-      
-    </div>
+    <form className='container' onSubmit={handleSubmit}>
+      <input
+        className='block w-full mb-5 rounded-md border-0 py-2.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-xl 2xl:leading-6'
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <input
+        className='block w-full mb-5 rounded-md border-0 py-2.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-xl 2xl:leading-6'
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button
+        className='p-5 pl-20 pr-20 m-5 mx-40 rounded-md border-0 text-blue-900 ring-1 ring-inset ring-blue-300 text-xl 2xl:leading-8'
+        type="submit">Je me connecte</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
   );
 };
+
 export default Login;
